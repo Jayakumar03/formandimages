d6485f5bc0372307a268148bc619f0e1afb50512
jayakumar03 <jaimech61@gmail.com>
2023-08-08 10:37:40 +0530

    Added cloudinary api and logic to upload file

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-const express = require("express")
-const fileUpload = require("express-fileupload")
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const cloudinary = require("cloudinary").v2; // api has switched to version 2
 
-const app = express()
+const app = express();
 
 // Default view engine
 app.set("view engine", "ejs");
 
+// Config
+// ! Api issue
+cloudinary.config({
+  cloud_name: "70lue1ntc",
+  api_key: "public_jdOpMOMvq7eJaBiojgYswsgfezk=",
+  api_secret: "private_RdvkrkuCOCt6fwvsMpo17xHjDYA=",
+});
+
 // Middle ware
-app.use(express.json()) 
+app.use(express.json());
 /*
 app.use(express.json()):
 
@@ -15,7 +24,7 @@ Purpose: This middleware is used to parse incoming JSON data from the request bo
 Data Type: It handles requests with the Content-Type: application/json header and parses the JSON data into a JavaScript object.
 Usage: It's commonly used when clients send data in JSON format, which is a common practice in modern APIs.
 */
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 /*
 app.use(express.urlencoded({ extended: true })):
 
@@ -24,11 +33,12 @@ Data Type: It handles requests with the Content-Type: application/x-www-form-url
 Usage: It's commonly used when data is sent from HTML forms.
 */
 
-
-app.use(fileUpload({
+app.use(
+  fileUpload({
     useTempFiles: true,
-    tempFileDir : "/tmp/"
-}))
+    tempFileDir: "/tmp/",
+  })
+);
 
 /*
 Using the fileUpload Middleware: This line of code tells your Express app to use the fileUpload middleware. The middleware is configured with an options object passed as an argument. In this case, you're setting two options:
@@ -40,43 +50,54 @@ If useTempFiles: true, tempFileDir : "/tmp/" are used it wont provide the buffer
 
 */
 
-
-
 // Routes
 app.get("/myget", (req, res) => {
+  // res.send(req.body)
+  // won't work if your view engine is from ejs. But it will work for postman and react any other frontend frameworks
 
-    // res.send(req.body)
-    // won't work if your view engine is from ejs. But it will work for postman and react any other frontend frameworks
+  // USe have use query instaed of body. But this method will not work in react, vue or postman
+  res.send(req.query);
+  console.log(req.query.files);
+});
 
-    // USe have use query instaed of body. But this method will not work in react, vue or postman
-    res.send(req.query) 
-    console.log(req.query.files)
+// ? logic working. But file uploading is not takening palce due to cloudinary api issue
+app.post("/mypost", async (req, res) => {
+  console.log(req.body);
 
-})
+  let file = req.files.sampleFile;
+  //   console.log(file);
 
-app.post("/mypost", (req, res) => {
+  let result = cloudinary.uploader
+    .upload(file.tempFilePath, {
+      folder: "users",
+    })
+    .then(() => {
+      console.log("SuccessFully uploaded the image");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 
-    res.send(req.body)
-     console.log(req.files)
+  let details = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    result: result,
+  };
 
-})
-
+  res.send(details);
+});
 
 // serving the frontend
-
 app.get("/mygetform", (req, res) => {
-    // By default when used render it check in views folder. so only provide the html file name as argument
+  // By default when used render it check in views folder. so only provide the html file name as argument
 
-    res.render("getform")
-
-})
+  res.render("getform");
+});
 
 app.get("/mypostform", (req, res) => {
-    // By default when used render it check in views folder. so only provide the html file name as argument
+  // By default when used render it check in views folder. so only provide the html file name as argument
 
-    res.render("postform")
+  res.render("postform");
+});
 
-})
-
-
-module.exports = app
+module.exports = app;
